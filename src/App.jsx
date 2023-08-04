6c723a3067e3b8367af33fb6fd5151d998bbd333
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-08-04 15:31:48 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Landing'
+import Signup from './Signup';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Home from './Home'
 
 
 function App() {
-  
 
   return (
     <>
-      
+    <div style={{height:"100vh",
+  width:"100vw"}}>
+
+      <Router>
+        <Routes>
+        <Route path={"/"} element={<Home />} />
+        
+        </Routes>
+      </Router>
+
+    </div>
+     
     </>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div style={{backgroundColor:'#292929',color:'#fffffe',width:'1850px',height:'900px',paddingTop:0,marginTop:0}}>
+            <h1>Welcome to leetcode</h1>
+        </div>
+
+
+    ) 
+}
+
+export default Home
